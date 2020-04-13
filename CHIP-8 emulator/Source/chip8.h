c8c3b0ac2f817d94f5165e94d7953650a9e51a35
ankien <andrew_kien12@yahoo.com>
2020-04-13 18:55:51 -0400

    worked on the wikipedia specifications

--- CHIP-8 emulator/Source/chip8.h
+++ CHIP-8 emulator/Source/chip8.h
@@ -2,7 +2,35 @@
 #define CHIP8_H
 
 class Chip8 {
+
+    /* RAM */
+    // 4096 bytes of memory
+    unsigned char  memory[4096];
+
+    /* CPU */
+    // 2 byte long opcodes
+    unsigned short opcode;
+    // 16 byte sized registers
+    unsigned char reg[16];
+    // stack
+    unsigned short stack[16];
+    unsigned short sp;
+    // index register + program counter
+    // 0x000-0x1FF - Chip 8 interpreter (contains font set in emu)
+    // 0x050 - 0x0A0 - Used for the built in 4x5 pixel font set(0 - F)
+    // 0x200 - 0xFFF - Program ROM and work RAM
+    unsigned short I, pc;
+
+    /* Display */
+    // 64 x 32 pixel res
+    int screen[64 * 32]; // could use a vector or multi-d array for this
+
+    /* Sound */
     
+
+public:
+
+    ~Chip8();
 };
 
 #endif
