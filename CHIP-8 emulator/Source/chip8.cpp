d4a46d789724898d339f4103e7548c259b18603e
ankien <andrew_kien12@yahoo.com>
2020-04-20 14:42:47 -0400

    finished boilerplate (hopefully)

--- CHIP-8 emulator/Source/chip8.cpp
+++ CHIP-8 emulator/Source/chip8.cpp
@@ -1,26 +1,78 @@
 #include "chip8.h" // CPU implementation
 #include <cstring>
 #include <fstream>
+#include <time.h>
 
-/* Constructor */
+Chip8::Chip8() { // Prepare system state before emulation cycles
 
-Chip8::Chip8() {
-
+    pc     = 0x200;  // Program counter starts at 0x200
+    opcode = 0;      // Reset current opcode	
+    I      = 0;      // Reset index register
+    sp     = 0;      // Reset stack pointer
     
+    // Clear display
+    for(int i = 0; i < 2048; i++) {
+        screen[i] = 0;
+    }
+    // Clear stack
+    for(int i = 0; i < 16; i++) {
+        stack[i] = 0;
+    }
+    // Clear registers V0-VF
+    for (int i = 0; i < 16; i++) {
+        V[i] = 0;
+    }
+    // Clear memory
+    for (int i = 0; i < 4096; i++) {
+        memory[i] = 0;
+    }
+
+    unsigned char chip8Fontset[80] = {
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    };
+
+    // Load fontset
+    for (int i = 0; i < 80; i++) {
+        memory[i] = chip8Fontset[i];
+    }
+
+    // Reset timers
+    delayTimer = 0;
+    soundTimer = 0;
+
+    // Clear screen
+    drawFlag = true;
+
+    srand(time(NULL)); // Seed for opcode
 }
 
 /* Member Methods */
 
-void Chip8::initialize() {
-
-}
-
+// Emulates one cycle: Fetch, Decode, then Execute opcode; update timer afterwards
 void Chip8::emulateCycle() {
+    // Fetch 2 byte opcode
+    unsigned short opcode = (memory[pc] << 8) | memory[pc + 1];
+    
+    // Decode opcode
 
+    // Execute opcode
+
+    // Update timers
 }
-
-/* Destructor */
 
 Chip8::~Chip8() {}
-
-/* Helper Functions */
--- CHIP-8 emulator/Source/chip8.h
+++ CHIP-8 emulator/Source/chip8.h
@@ -4,59 +4,47 @@
 #include <cstdint>
 
 class Chip8 {
+    uint8_t  memory[4096]; // 4096 bytes of memory
 
-    /* RAM */
-
-    // 4096 bytes of memory
-    uint8_t  memory[4096];
-
-    /* CPU */
-
-    // 2 byte long opcodes
-    uint16_t opcode;
+    uint16_t opcode; // 2 byte long opcodes
 
     // 16 byte sized registers
     // 15 general purpose registers (V0 - VE), one carry flag
     uint8_t V[16];
 
-    // stack
+    // Stack
     uint16_t stack[16];
     uint16_t sp;
 
-    // index register + program counter (range of 0x000 - 0xFFF)
+    // Index register + program counter (range of 0x000 - 0xFFF)
     // System Memory Map:
     // 0x000 - 0x1FF - Chip 8 interpreter (contains font set in emu)
     // 0x050 - 0x0A0 - Used for the built in 4x5 pixel font set(0 - F)
     // 0x200 - 0xFFF - Program ROM and work RAM
     uint16_t I, pc;
 
-    /* Display */
+    uint8_t screen[64 * 32]; // 64 x 32 (2048) pixel res, state array (1 or 0)
+    bool drawFlag; // Need to draw if true
 
-    // 64 x 32 pixel res, state array (1 or 0)
-    uint8_t screen[64 * 32];
-
-    /* Sound */
-
-    // two timer registers that count at 60hz, when set they count down to zero
+    // Two timer registers that count at 60hz, when set they count down to zero
     uint8_t delayTimer;
     uint8_t soundTimer;
 
-    /* Keypad */
-
-    // hex based keypad, each key stores a state
-    uint8_t key[16];
+    uint8_t key[16]; // Hex based keypad, each key stores a state
 
 public:
-
     Chip8();
     
     bool loadRom(std::string);
-    
-    // initialized memory and registers once
-    void initialize();
 
-    // emulates one cycle: Fetch, Decode, then Execute opcode; update timer afterwards
-    void emulateCycle();
+    bool getDrawFlag();
+    void setDrawFlag(bool);
+
+    uint8_t getDisplayValue(int);
+
+    uint8_t setKey(int, uint8_t);
+
+    void emulateCycle(); // Emulates one cycle: Fetch, Decode, then Execute opcode; update timer afterwards
 
     ~Chip8();
 };
